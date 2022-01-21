54c7cd4e1390cd4f2509286da4f733dca872a297
freyarowland <frowland@usgs.gov>
2022-01-21 17:13:04 -0600

    Code clean up

--- code/NeighborhoodCompCode.R
+++ code/NeighborhoodCompCode.R
@@ -14,6 +14,10 @@ library(reshape2)
 library(stringr)
 library(dplyr)
 
+# load pond data
+# no coordinates included because of sensitive populations
+# put "distance.mat.csv" has all of the distances between ponds
+coords <- read.csv("pondinfo.csv", header = TRUE)
 
 # load distance matrix (see spatial synchrony code)
 distance.mat.m <- read.csv("distance.mat.csv", header = TRUE)
@@ -35,12 +39,6 @@ for (r in 1:nrow(distance.mat.500)) {
 conn.mat <- ifelse(test = distance.mat.m <= 500, 1, NA)
 diag(conn.mat) <- NA
 
-# Nearest neighbor (nn) measurment "nn_mat"
-# return only smallest value in each column ----
-# load data
-nn.mat <- read.csv("nn_mat.csv", header = TRUE)
-rownames(nn.mat) <- rownames(distance.mat.m)
-colnames(nn.mat) <- colnames(distance.mat.m)
 
 # add in row and column names ----
 rownames(conn.mat) <- rownames(distance.mat.500)
@@ -51,28 +49,22 @@ diag(conn.mat) <- NA
 
 # multiply entry by pond average egg masses
 conn.df <- data.frame(conn.mat)
-nn.df <- data.frame(nn.mat)
-
-
 
 # changing data so column is RASY count within that year ----
 
 # fill in missing dates with NA
-eggs_long <- complete(eggs2, Year=2000:2020,
+eggs_long <- complete(alldata2, Year=2000:2020,
                       fill = list(Avg.RASY.Count = NA))
 
 # recast data from long to wide format
-egg_wide2 <- dcast(eggs_long, Pond.ID ~ Year, value.var="Avg.RASY.Count", mean, na.rm = TRUE)
+egg_wide2 <- reshape2::dcast(eggs_long, Pond.ID ~ Year, value.var="Avg.RASY.Count", mean, na.rm = TRUE)
 
 # some clean up
 egg_wide2[egg_wide2 =="NaN"] <- "NA"
 egg_wide3 <- egg_wide2 %>% mutate_if(is.character,as.numeric)
 
-# example of how to use
-# Moran2000 <- Moran.I(egg_wide3$"2000", pond.dist.inv, scaled = TRUE, na.rm = TRUE)
-
 # make different years of egg mass counts
-eggsavg <- eggs11 %>%
+eggsavg <- alldata2 %>%
   dplyr::group_by(Pond.ID) %>%
   dplyr::summarize(avg = mean(Avg.RASY.Count, na.rm = TRUE))
 
@@ -100,28 +92,6 @@ conn_2018_eggs <- egg_wide3$"2018" * conn.df
 conn_2019_eggs <- egg_wide3$"2019" * conn.df
 conn_2020_eggs <- egg_wide3$"2020" * conn.df
 
-conn_2000_eggs <- egg_wide3$"2000" * nn.df
-conn_2001_eggs <- egg_wide3$"2001" * nn.df
-conn_2002_eggs <- egg_wide3$"2002" * nn.df
-conn_2003_eggs <- egg_wide3$"2003" * nn.df
-conn_2004_eggs <- egg_wide3$"2004" * nn.df
-conn_2005_eggs <- egg_wide3$"2005" * nn.df
-conn_2006_eggs <- egg_wide3$"2006" * nn.df
-conn_2007_eggs <- egg_wide3$"2007" * nn.df
-conn_2008_eggs <- egg_wide3$"2008" * nn.df
-conn_2009_eggs <- egg_wide3$"2009" * nn.df
-conn_2010_eggs <- egg_wide3$"2010" * nn.df
-conn_2011_eggs <- egg_wide3$"2011" * nn.df
-conn_2012_eggs <- egg_wide3$"2012" * nn.df
-conn_2013_eggs <- egg_wide3$"2013" * nn.df
-conn_2014_eggs <- egg_wide3$"2014" * nn.df
-conn_2015_eggs <- egg_wide3$"2015" * nn.df
-conn_2016_eggs <- egg_wide3$"2016" * nn.df
-conn_2017_eggs <- egg_wide3$"2017" * nn.df
-conn_2018_eggs <- egg_wide3$"2018" * nn.df
-conn_2019_eggs <- egg_wide3$"2019" * nn.df
-conn_2020_eggs <- egg_wide3$"2020" * nn.df
-
 connect_final <- data.frame(dplyr::as_tibble(rowSums(x = conn_x_eggs, na.rm = TRUE)))
 connect_2000 <- data.frame(dplyr::as_tibble(rowSums(x = conn_2000_eggs, na.rm = TRUE)))
 connect_2001 <- data.frame(dplyr::as_tibble(rowSums(x = conn_2001_eggs, na.rm = TRUE)))
@@ -146,27 +116,27 @@ connect_2019 <- data.frame(dplyr::as_tibble(rowSums(x = conn_2019_eggs, na.rm = 
 connect_2020 <- data.frame(dplyr::as_tibble(rowSums(x = conn_2020_eggs, na.rm = TRUE)))
 
 # connect pond.ID to data ----
-connect_2000$Pond.ID <- coords$name
-connect_2001$Pond.ID <- coords$name
-connect_2002$Pond.ID <- coords$name
-connect_2003$Pond.ID <- coords$name
-connect_2004$Pond.ID <- coords$name
-connect_2005$Pond.ID <- coords$name
-connect_2006$Pond.ID <- coords$name
-connect_2007$Pond.ID <- coords$name
-connect_2008$Pond.ID <- coords$name
-connect_2009$Pond.ID <- coords$name
-connect_2010$Pond.ID <- coords$name
-connect_2011$Pond.ID <- coords$name
-connect_2012$Pond.ID <- coords$name
-connect_2013$Pond.ID <- coords$name
-connect_2014$Pond.ID <- coords$name
-connect_2015$Pond.ID <- coords$name
-connect_2016$Pond.ID <- coords$name
-connect_2017$Pond.ID <- coords$name
-connect_2018$Pond.ID <- coords$name
-connect_2019$Pond.ID <- coords$name
-connect_2020$Pond.ID <- coords$name
+connect_2000$Pond.ID <- coords$Pond.ID
+connect_2001$Pond.ID <- coords$Pond.ID
+connect_2002$Pond.ID <- coords$Pond.ID
+connect_2003$Pond.ID <- coords$Pond.ID
+connect_2004$Pond.ID <- coords$Pond.ID
+connect_2005$Pond.ID <- coords$Pond.ID
+connect_2006$Pond.ID <- coords$Pond.ID
+connect_2007$Pond.ID <- coords$Pond.ID
+connect_2008$Pond.ID <- coords$Pond.ID
+connect_2009$Pond.ID <- coords$Pond.ID
+connect_2010$Pond.ID <- coords$Pond.ID
+connect_2011$Pond.ID <- coords$Pond.ID
+connect_2012$Pond.ID <- coords$Pond.ID
+connect_2013$Pond.ID <- coords$Pond.ID
+connect_2014$Pond.ID <- coords$Pond.ID
+connect_2015$Pond.ID <- coords$Pond.ID
+connect_2016$Pond.ID <- coords$Pond.ID
+connect_2017$Pond.ID <- coords$Pond.ID
+connect_2018$Pond.ID <- coords$Pond.ID
+connect_2019$Pond.ID <- coords$Pond.ID
+connect_2020$Pond.ID <- coords$Pond.ID
 
 # make this into one big dataframe to merge with other data ----
 dd1 <- left_join(connect_2000, connect_2001, by = "Pond.ID")
@@ -224,9 +194,9 @@ dd20n <- dd20 %>%
   )
 
 # recast from long to wide ----
-dd_final <- melt(dd20n, id.vars = "Pond.ID",
+dd_final <- reshape2::melt(dd20n, id.vars = "Pond.ID",
                  variable.name = "wYear",
-                 value.name = "nnEggs")
+                 value.name = "wDD")
 head(dd_final)
 
 
@@ -236,13 +206,7 @@ numextract <- function(string){
   str_extract(string, "\\-*\\d+\\.*\\d*")
 }
 dd_final$Year <- numextract(dd_final$wYear)
-wDDfinal$Year <- numextract(wDDfinal$wYear)
-nn_final$Year <- numextract(nn_final$wYear)
 
 # save data
-
 # weighted density dependence
-write.csv(x = wDDfinal, file="wDDdata.csv")
-
-# nearest neighbor
-write.csv(nn_final, "nn_final.csv")
+write.csv(x = dd_final, file="wDDdata.csv")
